1e96418500d218988d803f7a7f83e2e7cad79493
kelvin <kval0003@student.monash.edu>
2023-08-20 20:49:04 +1000

    added classes

--- electromagnet.py
+++ electromagnet.py
@@ -1,30 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Set the GPIO pin for the relay
-relay_pin = 17
-
-# Initialize the relay pin as an output pin
-GPIO.setup(relay_pin, GPIO.OUT)
-
-def turn_on_relay():
-    GPIO.output(relay_pin, GPIO.HIGH)
-    print("Relay ON")
-
-def turn_off_relay():
-    GPIO.output(relay_pin, GPIO.LOW)
-    print("Relay OFF")
+class ElectromagnetController:
+    def __init__(self, relay_pin):
+        self.relay_pin = relay_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.relay_pin, GPIO.OUT)
+        self.electromagnet_state = False  # Initially turned off
+        
+    def turn_on(self):
+        GPIO.output(self.relay_pin, GPIO.HIGH)
+        self.electromagnet_state = True
+        print("Relay ON")
+        
+    def turn_off(self):
+        GPIO.output(self.relay_pin, GPIO.LOW)
+        self.electromagnet_state = False
+        print("Relay OFF")
+        
+    def toggle(self):
+        if self.electromagnet_state:
+            self.turn_off()
+        else:
+            self.turn_on()
+        
+    def cleanup(self):
+        GPIO.cleanup()
 
 try:
+    relay_pin = 17
+    magnet_controller = ElectromagnetController(relay_pin)
+    
     while True:
-        turn_on_relay()
-        time.sleep(20)  # Electromagnet ON for 2 seconds
-        turn_off_relay()
-        time.sleep(2)  # Electromagnet OFF for 2 seconds
-
+        user_input = input("Press '1' to toggle the electromagnet, 'q' to quit: ")
+        if user_input == '1':
+            magnet_controller.toggle()
+        elif user_input == 'q':
+            break
+    
 except KeyboardInterrupt:
-    print("Exiting...")
-    GPIO.cleanup()
+    pass
+finally:
+    magnet_controller.cleanup()
--- stepper.py
+++ stepper.py
@@ -1,66 +1,100 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define the GPIO pins for the L289N driver
-IN1 = 17
-IN2 = 18
-IN3 = 27
-IN4 = 22
-
-# Set the GPIO mode and pins as outputs
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
-
-# Define the motor control sequence
-# (You may need to adjust the sequence based on your motor and driver wiring)
-sequence = [
-    [1, 0, 0, 1],
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1]
-]
-
-def set_step(w1, w2, w3, w4):
-    GPIO.output(IN1, w1)
-    GPIO.output(IN2, w2)
-    GPIO.output(IN3, w3)
-    GPIO.output(IN4, w4)
-
-def forward(delay, steps):
-    for _ in range(steps):
-        for step in sequence:
-            set_step(*step)
-            time.sleep(delay)
-
-def backward(delay, steps):
-    for _ in range(steps):
-        for step in reversed(sequence):
-            set_step(*step)
-            time.sleep(delay)
+class MotorController:
+    def __init__(self, in1, in2, in3, in4):
+        self.IN1 = in1
+        self.IN2 = in2
+        self.IN3 = in3
+        self.IN4 = in4
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        
+        self.sequence = [
+            [1, 0, 0, 1],
+            [1, 0, 0, 0],
+            [1, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1]
+        ]
+    
+    def set_step(self, w1, w2, w3, w4):
+        GPIO.output(self.IN1, w1)
+        GPIO.output(self.IN2, w2)
+        GPIO.output(self.IN3, w3)
+        GPIO.output(self.IN4, w4)
+    
+    def move(self, delay, steps, direction):
+        if direction == "forward":
+            step_sequence = self.sequence
+        elif direction == "backward":
+            step_sequence = reversed(self.sequence)
+        else:
+            raise ValueError("Invalid direction")
+        
+        for _ in range(steps):
+            for step in step_sequence:
+                self.set_step(*step)
+                time.sleep(delay)
+    
+    
+    def cleanup(self):
+        GPIO.cleanup()
 
 try:
-    # You can adjust the delay and steps according to your motor and requirements
+    # Create instances for each motor with their respective pins
+    motor1 = MotorController(in1=17, in2=18, in3=27, in4=22)
+    motor2 = MotorController(in1=23, in2=24, in3=25, in4=26)
+
     delay = 0.005
-    steps = 200  # Number of steps to move the motor
+    steps = 150  # Number of steps to move the motor
 
-    # Move the motor forward
-    forward(delay, steps)
+    # Move the motors forward
+    motor1.move(delay, steps, "forward")
+    motor2.move(delay, steps, "forward")
 
-    # Pause for a moment
     time.sleep(1)
 
-    # Move the motor backward
-    backward(delay, steps)
+    # Move the motors backward
+    motor1.move(delay, steps, "backward")
+    motor2.move(delay, steps, "backward")
 
+    time.sleep(1)
+
+    # Move the motors backward
+    motor1.move(delay, steps, "forward")
+    motor2.move(delay, steps, "backward")
+
+    time.sleep(1)
+
+    # Move the motors backward
+    motor1.move(delay, steps, "backward")
+    motor2.move(delay, steps, "forward")
+
+    time.sleep(1)
+
+    # Move the motors backward
+    motor1.move(delay, steps, "forward")
+
+    time.sleep(1)
+
+    motor1.move(delay, steps, "backward")
+
+    time.sleep(1)
+    
+    motor1.move(delay, steps, "forward")
+
+    time.sleep(1)
+    motor2.move(delay, steps, "backward")
 except KeyboardInterrupt:
     pass
 finally:
-    # Clean up the GPIO on exit
-    GPIO.cleanup()
+    motor1.cleanup()
+    motor2.cleanup()
