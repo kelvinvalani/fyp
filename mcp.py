cca1c34ea62dce3d15eaf818c7d20a6bc3a20df3
kelvinvalani <kelvin.valani@gmail.com>
2023-08-22 23:11:11 +1000

    mcp final

--- mcp.py
+++ mcp.py
@@ -2,30 +2,37 @@ import spidev
 import time
 
 # Define MCP23S17 registers
+IODIRB = 0x01  # I/O Direction register for port B
 GPIOB = 0x13   # Input/output register for port B
+GPPUB = 0x0D   # Pull-Up resistor enable register for port B
 
-# Specify the pin to check
-CHECK_PIN = 0  # Pin B0
+# Specify the pin to read
+READ_PIN = 0   # Pin B0
 
 # SPI setup
 spi = spidev.SpiDev()
-spi.open(0, 1)  # Use SPI bus 0, device 0
+spi.open(0, 0)  # Use SPI bus 0, device 0
 spi.max_speed_hz = 1000000  # Set SPI speed (can adjust as needed)
+
+# Configure the specified pin as input
+spi.xfer2([0x40, IODIRB, 0x01])  # Set pin B0 as input
+
+# Enable the pull-up resistor for the specified pin
+spi.xfer2([0x40, GPPUB, 0x01])  # Enable pull-up for pin B0
 
 # Read the state of the specified pin
 def read_pin_state(pin):
     data = spi.xfer2([0x41, GPIOB, 0x00])  # Read port B data
-    print(data)
     return (data[2] >> pin) & 1  # Extract the state of the specified pin
 
 try:
     while True:
-        pin_state = read_pin_state(CHECK_PIN)
+        pin_state = read_pin_state(READ_PIN)
         
-        # if pin_state == 1:
-        #     print("Pin is high.")
-        # else:
-        #     print("Pin is low.")
+        if pin_state == 1:
+            print("Pin is high.")
+        else:
+            print("Pin is low.")
         
         time.sleep(0.5)
         
