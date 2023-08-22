a56b5ed89a76b442117ea9426194b64ec1ee261d
kelvinvalani <kelvin.valani@gmail.com>
2023-08-22 23:32:24 +1000

    simple librAYR

--- led.py
+++ led.py
@@ -1,19 +1,24 @@
-import RPi.GPIO as GPIO
-import time
 from RPiMCP23S17.MCP23S17 import MCP23S17
 import time
 
-mcp = MCP23S17(0,0)
-mcp.open()
+mcp1 = MCP23S17(bus=0x00, pin_cs=0x00, device_id=0x00)
+
+mcp1.open()
+
+
 for x in range(0, 16):
-    mcp.setDirection(x, mcp.DIR_INPUT)
-    mcp.setPullupMode(x,MCP23S17.PULLUP_ENABLED)
+    mcp1.setDirection(x, mcp1.DIR_INPUT)
+    mcp1.setPullupMode(x,MCP23S17.PULLUP_ENABLED)
+
+
+
 try:
     while True:
         # Read the state of the GPIO pin
-        sensor_state = mcp.digitalRead(0)
+
+        sensor_state = mcp1.digitalRead(8)
         
-        if sensor_state == MCP23S17.LEVEL_LOW:
+        if sensor_state == MCP23S17.LEVEL_HIGH:
             print("No magnetic field")
         else:
             print("Magnetic field detected!")
@@ -24,4 +29,4 @@ except KeyboardInterrupt:
     pass
 
 # Clean up GPIO configuration
-GPIO.cleanup()
+mcp1.close
