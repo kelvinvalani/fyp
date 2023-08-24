ce164e5607da0250a2555e299fa09757627b5aef
kelvinvalani <kelvin.valani@gmail.com>
2023-08-25 01:19:01 +1000

    calibrated steppers

--- stepper.py
+++ stepper.py
@@ -2,17 +2,25 @@ import RPi.GPIO as GPIO
 import time
 
 class MotorController:
-    def __init__(self, in1, in2, in3, in4):
-        self.IN1 = in1
-        self.IN2 = in2
-        self.IN3 = in3
-        self.IN4 = in4
-        
+    def __init__(self, motor1_in1, motor1_in2, motor1_in3, motor1_in4,motor2_in1, motor2_in2, motor2_in3, motor2_in4):
+        self.MOTOR1_IN1 = motor1_in1
+        self.MOTOR1_IN2 = motor1_in2
+        self.MOTOR1_IN3 = motor1_in3
+        self.MOTOR1_IN4 = motor1_in4
+        self.MOTOR2_IN1 = motor2_in1
+        self.MOTOR2_IN2 = motor2_in2
+        self.MOTOR2_IN3 = motor2_in3
+        self.MOTOR2_IN4 = motor2_in4
+
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.IN1, GPIO.OUT)
-        GPIO.setup(self.IN2, GPIO.OUT)
-        GPIO.setup(self.IN3, GPIO.OUT)
-        GPIO.setup(self.IN4, GPIO.OUT)
+        GPIO.setup(self.MOTOR1_IN1, GPIO.OUT)
+        GPIO.setup(self.MOTOR1_IN2, GPIO.OUT)
+        GPIO.setup(self.MOTOR1_IN3, GPIO.OUT)
+        GPIO.setup(self.MOTOR1_IN4, GPIO.OUT)
+        GPIO.setup(self.MOTOR2_IN1, GPIO.OUT)
+        GPIO.setup(self.MOTOR2_IN2, GPIO.OUT)
+        GPIO.setup(self.MOTOR2_IN3, GPIO.OUT)
+        GPIO.setup(self.MOTOR2_IN4, GPIO.OUT)
         
         self.sequence = [
             [1, 0, 0, 1],
@@ -25,24 +33,54 @@ class MotorController:
             [0, 0, 0, 1]
         ]
     
-    def set_step(self, w1, w2, w3, w4):
-        GPIO.output(self.IN1, w1)
-        GPIO.output(self.IN2, w2)
-        GPIO.output(self.IN3, w3)
-        GPIO.output(self.IN4, w4)
+    def set_step(self, motor1_w1, motor1_w2, motor1_w3, motor1_w4,motor2_w1, motor2_w2, motor2_w3, motor2_w4):
+
+        if motor1_w1 != "none":
+            GPIO.output(self.MOTOR1_IN1, motor1_w1)
+            GPIO.output(self.MOTOR1_IN2, motor1_w2)
+            GPIO.output(self.MOTOR1_IN3, motor1_w3)
+            GPIO.output(self.MOTOR1_IN4, motor1_w4)
+
+        if motor2_w1 != "none":
+            GPIO.output(self.MOTOR2_IN1, motor2_w1)
+            GPIO.output(self.MOTOR2_IN2, motor2_w2)
+            GPIO.output(self.MOTOR2_IN3, motor2_w3)
+            GPIO.output(self.MOTOR2_IN4, motor2_w4)
     
-    def move(self, delay, steps, direction):
-        if direction == "forward":
-            step_sequence = self.sequence
-        elif direction == "backward":
-            step_sequence = reversed(self.sequence)
+    def move(self, delay, steps, motor1_direction,motor2_direction):
+        if motor1_direction == "forward":
+            motor1_step_sequence = self.sequence
+        elif motor1_direction == "backward":
+            motor1_step_sequence = self.sequence[::-1]
         else:
-            raise ValueError("Invalid direction")
+            motor1_step_sequence = []
+
+        if motor2_direction == "forward":
+            motor2_step_sequence = self.sequence
+        elif motor2_direction == "backward":
+            motor2_step_sequence = self.sequence[::-1]
+        else:
+            motor2_step_sequence = []
         
-        for _ in range(steps):
-            for step in step_sequence:
-                self.set_step(*step)
-                time.sleep(delay)
+        if len(motor1_step_sequence)>0 and len(motor2_step_sequence):
+            for _ in range(steps):
+                for i in range(len(motor1_step_sequence)):
+                    self.set_step(*motor1_step_sequence[i],*motor2_step_sequence[i])
+                    time.sleep(delay)
+        elif len(motor1_step_sequence)>0:
+            for _ in range(steps):
+                for i in range(len(motor1_step_sequence)):
+                    self.set_step(*motor1_step_sequence[i],"none","none","none","none")
+                    time.sleep(delay)
+        elif len(motor2_step_sequence)>0:
+            for _ in range(steps):
+                for i in range(len(motor2_step_sequence)):
+                    self.set_step("none","none","none","none",*motor2_step_sequence[i])
+                    time.sleep(delay)
+        else:
+            pass
+
+
     
     
     def cleanup(self):
@@ -50,51 +88,41 @@ class MotorController:
 
 try:
     # Create instances for each motor with their respective pins
-    motor1 = MotorController(in1=17, in2=18, in3=27, in4=22)
-    motor2 = MotorController(in1=23, in2=24, in3=25, in4=26)
+    gantry = MotorController(motor1_in1=17, motor1_in2=18, motor1_in3=27, motor1_in4=22,motor2_in1=19, motor2_in2=26, motor2_in3=16, motor2_in4=20)
 
-    delay = 0.005
-    steps = 150  # Number of steps to move the motor
+    delay = 0.001
+    steps = 100  # Number of steps to move the motor
 
-    # Move the motors forward
-    motor1.move(delay, steps, "forward")
-    motor2.move(delay, steps, "forward")
+    # 8cm = 100 steps when moveing in perpendicular
 
-    time.sleep(1)
-
-    # Move the motors backward
-    motor1.move(delay, steps, "backward")
-    motor2.move(delay, steps, "backward")
-
-    time.sleep(1)
-
-    # Move the motors backward
-    motor1.move(delay, steps, "forward")
-    motor2.move(delay, steps, "backward")
-
-    time.sleep(1)
-
-    # Move the motors backward
-    motor1.move(delay, steps, "backward")
-    motor2.move(delay, steps, "forward")
-
-    time.sleep(1)
-
-    # Move the motors backward
-    motor1.move(delay, steps, "forward")
-
-    time.sleep(1)
-
-    motor1.move(delay, steps, "backward")
-
-    time.sleep(1)
+    # 5.67cm = 100 steps when moving diaganol
     
-    motor1.move(delay, steps, "forward")
+    # gantry.move(delay, steps, "forward","forward")    # Move up
+    # time.sleep(1)
+    # gantry.move(delay, steps, "backward","backward") # Move down
+    # time.sleep(1)
+    # gantry.move(delay, steps, "forward","backward") # Move left
+    # time.sleep(1)
+    # gantry.move(delay, steps, "backward","forward") # Move right
+    # time.sleep(1)
+    # gantry.move(delay, steps, "forward","none") # move north west
+    # time.sleep(1)
+    # gantry.move(delay, steps, "none","forward") # move north east
+    # time.sleep(1)
+    # gantry.move(delay, steps, "backward","none") # move south east
+    # time.sleep(1)
+    # gantry.move(delay, steps, "none","backward") # move south west
+
+    # gantry.move(delay, steps, "none","none") # stationary
+
+
+
+
+
+
 
     time.sleep(1)
-    motor2.move(delay, steps, "backward")
 except KeyboardInterrupt:
     pass
 finally:
-    motor1.cleanup()
-    motor2.cleanup()
+    gantry.cleanup()
