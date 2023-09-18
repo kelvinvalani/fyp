773121ea1454758dca12b9927ad260f0ff4e11f7
kelvin <kval0003@student.monash.edu>
2023-09-18 17:10:08 +1000

    changed magnet pin

--- driver.py
+++ driver.py
@@ -6,7 +6,7 @@ class Driver:
         self.gantry = MotorController(motor1_in1=2, motor1_in2=3, motor1_in3=4, motor1_in4=14,motor2_in1=19, motor2_in2=26, motor2_in3=16, motor2_in4=20)
         self.steps_per_square = 50
         self.steps_per_cm = 13
-        self.magnet = ElectromagnetController(17)
+        self.magnet = ElectromagnetController(13)
         self.location = location
         self.delay = 0.001
 
