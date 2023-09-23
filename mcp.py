0f75eb1dcdb6d2ec8938871d71c5f79a89703fea
kelvin <kval0003@student.monash.edu>
2023-09-23 19:59:58 +1000

    testing

--- mcp.py
+++ mcp.py
@@ -6,6 +6,9 @@ IODIRB = 0x01  # I/O Direction register for port B
 GPIOB = 0x13   # Input/output register for port B
 GPPUB = 0x0D   # Pull-Up resistor enable register for port B
 GPIOA = 0x12   # Input/output register for port A
+IODIRA = 0x00  # I/O Direction register for port A
+GPIOA = 0x12   # Input/output register for port A
+GPPUA = 0x0C   # Pull-Up resistor enable register for port A
 
 # Specify the pin to read
 READ_PIN = 0   # Pin B0
@@ -16,19 +19,32 @@ spi.open(0, 0)  # Use SPI bus 0, device 0
 spi.max_speed_hz = 1000000  # Set SPI speed (can adjust as needed)
 
 # Configure the specified pin as input
-spi.xfer2([0x40, IODIRB, 0x01])  # Set pin B0 as input
+spi.xfer2([0x40, IODIRB, 0xFF])  # Set pin B0 as input
 
 # Enable the pull-up resistor for the specified pin
-spi.xfer2([0x40, GPPUB, 0x01])  # Enable pull-up for pin B0
+spi.xfer2([0x40, GPPUB, 0xFF])  # Enable pull-up for pin B0
+
+# Configure the specified pin as input
+spi.xfer2([0x40, IODIRA, 0xFF])  # Set pin B0 as input
+
+# Enable the pull-up resistor for the specified pin
+spi.xfer2([0x40, GPPUA, 0xFF])  # Enable pull-up for pin B0
 
 # Read the state of the specified pin
-def read_pin_state(pin):
-    data = spi.xfer2([0x41, GPIOB, 0x00])  # Read port B data
+def read_pin_state(pin,port):
+    data = spi.xfer2([0x41, port, 0x00])  # Read port B data
     return (data[2] >> pin) & 1  # Extract the state of the specified pin
 
 try:
     while True:
-        pin_state = read_pin_state(READ_PIN)
+        pin_state = read_pin_state(READ_PIN,GPIOA)
+        
+        if pin_state == 1:
+            print("Pin is high.")
+        else:
+            print("Pin is low.")
+
+        pin_state = read_pin_state(READ_PIN,GPIOB)
         
         if pin_state == 1:
             print("Pin is high.")
