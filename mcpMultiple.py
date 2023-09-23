5e12c6fbc6933983dda93bf3fca3dd9addf5d82f
kelvin <kval0003@student.monash.edu>
2023-09-23 20:25:50 +1000

    test

--- mcpMultiple.py
+++ mcpMultiple.py
@@ -58,7 +58,7 @@ try:
     #         chessboardState.append(statesB)
     #         time.sleep(0.5)
     #     print(chessboardState)
-    chessboard = HallEffectBoard(0x60)
+    chessboard = HallEffectBoard(0xE0)
     while True:
         chessboardState = []
         statesA,statesB = chessboard.read_board()
