5e63fc3917b2fcded84f158c85e406e942ec3e85
kelvin <kval0003@student.monash.edu>
2023-10-05 11:12:08 +1100

    address testing

--- mcp.py
+++ mcp.py
@@ -9,7 +9,7 @@ GPIOA = 0x12   # Input/output register for port A
 IODIRA = 0x00  # I/O Direction register for port A
 GPIOA = 0x12   # Input/output register for port A
 GPPUA = 0x0C   # Pull-Up resistor enable register for port A
-address = 0x48
+address = 0x4E
 # Specify the pin to read
 READ_PIN = 0   # Pin B0
 
@@ -35,15 +35,15 @@ spi.xfer2([address, IODIRA, 0xFF])  # Set pin B0 as input
 spi.xfer2([address, GPPUA, 0xFF])  # Enable pull-up for pin B0
 
 # Read the state of the specified pin
-def read_pin_state(pin,port):
-    data = spi.xfer2([0x41, port, 0x00])  # Read port B data
+def read_pin_state(pin,port,address):
+    data = spi.xfer2([0x4F, port,0x00])  # Read port B data
     return (data[2] >> pin) & 1  # Extract the state of the specified pin
 
 
 if __name__ == "__main__":
     try:
         while True:
-            pin_state = read_pin_state(READ_PIN,GPIOB)
+            pin_state = read_pin_state(READ_PIN,GPIOB,address)
             
             if pin_state == 1:
                 print("Pin is high.")
