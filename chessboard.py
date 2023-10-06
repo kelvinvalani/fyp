18f368b3f23ec0ebcd4013931559616b3ff30fa7
kelvin <kval0003@student.monash.edu>
2023-10-06 14:14:09 +1100

    testing

--- chessboard.py
+++ chessboard.py
@@ -5,23 +5,23 @@ from detectionPCB import *
 class Chessboard:
     def __init__(self):
         self.columnAB = HallEffectBoard(0x40,0x41)
-        self.columnCD = HallEffectBoard(0x42,0x43)
-        self.columnEF = HallEffectBoard(0x44,0x45)
-        self.columnGH = HallEffectBoard(0x46,0x47)
-        self.columnIJ = HallEffectBoard(0x48,0x49)
-        self.columnKL = HallEffectBoard(0x4A,0x4B)
+        # self.columnCD = HallEffectBoard(0x42,0x43)
+        # self.columnEF = HallEffectBoard(0x44,0x45)
+        # self.columnGH = HallEffectBoard(0x46,0x47)
+        # self.columnIJ = HallEffectBoard(0x48,0x49)
+        # self.columnKL = HallEffectBoard(0x4A,0x4B)
 
     def read_board_state(self):
 
         columnA,columnB = self.columnAB.read_board()
-        columnC,columnD = self.columnCD.read_board()
-        columnE,columnF = self.columnEF.read_board()
-        columnG,columnH = self.columnGH.read_board()
-        columnI,columnJ = self.columnIJ.read_board()
-        columnK,columnL = self.columnKL.read_board()
+        # columnC,columnD = self.columnCD.read_board()
+        # columnE,columnF = self.columnEF.read_board()
+        # columnG,columnH = self.columnGH.read_board()
+        # columnI,columnJ = self.columnIJ.read_board()
+        # columnK,columnL = self.columnKL.read_board()
 
 
-        chessBoard = [columnB[::-1],columnA,columnD[::-1],columnC,columnF[::-1],columnE,columnH[::-1],columnG,columnJ[::-1],columnI,columnL[::-1],columnK]
+        chessBoard = [columnB[::-1],columnA]
 
         return chessBoard
 
