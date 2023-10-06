9c1b6c6be107cd643d985cf05f48e4fcccde6cf2
kelvinvalani <kelvin.valani@gmail.com>
2023-10-07 00:34:44 +1100

    testing

--- chessboard.py
+++ chessboard.py
@@ -6,19 +6,19 @@ class Chessboard:
     def __init__(self):
         self.columnAB = HallEffectBoard(0x40,0x41)
         self.columnCD = HallEffectBoard(0x42,0x43)
-        self.columnEF = HallEffectBoard(0x44,0x45)
-        self.columnGH = HallEffectBoard(0x46,0x47)
-        self.columnIJ = HallEffectBoard(0x48,0x49)
-        self.columnKL = HallEffectBoard(0x4A,0x4B)
+        # self.columnEF = HallEffectBoard(0x44,0x45)
+        # self.columnGH = HallEffectBoard(0x46,0x47)
+        # self.columnIJ = HallEffectBoard(0x48,0x49)
+        # self.columnKL = HallEffectBoard(0x4A,0x4B)
 
     def read_board_state(self):
 
         columnA,columnB = self.columnAB.read_board()
         columnC,columnD = self.columnCD.read_board()
-        columnE,columnF = self.columnEF.read_board()
-        columnG,columnH = self.columnGH.read_board()
-        columnI,columnJ = self.columnIJ.read_board()
-        columnK,columnL = self.columnKL.read_board()
+        # columnE,columnF = self.columnEF.read_board()
+        # columnG,columnH = self.columnGH.read_board()
+        # columnI,columnJ = self.columnIJ.read_board()
+        # columnK,columnL = self.columnKL.read_board()
 
 
         chessBoard = [columnA[::-1],columnB,columnC[::-1],columnD]
