4123c587c2d41dfb5e743262cf42eca48d59d211
kelvin <kval0003@student.monash.edu>
2023-10-06 14:18:36 +1100

    testing

--- chessboard.py
+++ chessboard.py
@@ -21,7 +21,7 @@ class Chessboard:
         # columnK,columnL = self.columnKL.read_board()
 
 
-        chessBoard = [columnA,columnB[::-1]]
+        chessBoard = [columnA[::-1],columnB]
 
         return chessBoard
 
