ffd8eba882454c935af5061444ec3e345865b2b2
kelvinvalani <kelvin.valani@gmail.com>
2023-10-16 16:24:16 +1100

    calib

--- driver.py
+++ driver.py
@@ -6,7 +6,7 @@ from limitSwitch import *
 class Driver:
     def __init__(self,location):
         self.gantry = MotorController(motor1_in1=2, motor1_in2=3, motor1_in3=4, motor1_in4=14,motor2_in1=19, motor2_in2=26, motor2_in3=16, motor2_in4=20)
-        self.steps_per_square = 50
+        self.steps_per_square = 49
         self.steps_per_cm = 12.55
         self.magnet = ElectromagnetController(13)
         self.location = location
