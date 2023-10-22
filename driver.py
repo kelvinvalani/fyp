882ca2b2a7e24cda4df7ad88ec3609ac43d90466
kelvin <kval0003@student.monash.edu>
2023-10-22 13:21:43 +1100

    test

--- driver.py
+++ driver.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         elif choice == "2":
             driver.relocalise()
         else:
-            driver.move_piece("B1","C8")
+            driver.move_piece("A1","A8")
 
 
 
