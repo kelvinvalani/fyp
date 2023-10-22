c0c6c76481a5b5a8dd097127dd9f90f4d48e13c3
kelvin <kval0003@student.monash.edu>
2023-10-22 13:26:53 +1100

    tets

--- driver.py
+++ driver.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         elif choice == "2":
             driver.relocalise()
         else:
-            driver.move_piece("A1","G1")
+            driver.move_piece("A1","C8")
 
 
 
